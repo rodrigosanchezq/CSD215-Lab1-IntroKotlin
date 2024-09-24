8b3416c0d765ce0137158bf75da829a9b28d2d21
Rodrigo Sanchez <23015753@saultcollege.ca>
2024-09-23 21:51:05 -0400

    code documentation

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,26 @@
 import java.io.File
 
+/**
+ * Data class to represent a task
+ * @param description The description of the task
+ * @param isComplete The status of the task (true if complete, false if not)
+ * @return Task object
+ */
+
 data class Task(val description: String, var isComplete: Boolean)
 
+/**
+ * variable file is a File object that represents the file (todo.txt) for upload/save the tasks list.
+ * variable toDo is a unmutable list of Task objects.
+ */
 val file = File("todo.txt")
+var toDo = loadFile()
 
+/**
+ * loads the tasks from the file
+ * @param file The file to load the tasks from
+ * @return List<Task> list of Task objects
+ */
 fun loadFile(): List<Task> {
     if (file.exists()) {
         val fileTasks = mutableListOf<Task>()
@@ -16,7 +33,12 @@ fun loadFile(): List<Task> {
         return emptyList()
     }
 }
-var toDo = loadFile()
+
+/**
+ * Saves the tasks to the file
+ * @param file The file to save the tasks to.
+ * @param toDo The list of Task objects to save to the file.
+ */
 
 fun saveFile() {
     file.writeText("")
@@ -24,6 +46,11 @@ fun saveFile() {
         file.appendText("${task.description},${task.isComplete}\n")
     }
 }
+
+/**
+ * Displays the list of tasks
+ * @param toDo The list of Task objects to display
+ */
 
 fun showList() {
     println("\nTasks TO DO:")
@@ -35,6 +62,12 @@ fun showList() {
         }
     }
 }
+
+/**
+ * Marks a task as complete in the list based in the user input.
+ * @param toDo The list of Task objects
+ * @param number The number of the task to mark as complete
+ */
 
 fun markTaskAsComplete() {
     print("Enter the task number to mark as complete: ")
@@ -54,6 +87,11 @@ fun markTaskAsComplete() {
     }
 }
 
+/**
+ * Adds a new task in the list.
+ * @param toDo The list of Task objects
+ */
+
 fun addTask() {
     val workList = toDo.toMutableList()
     println("Enter the task description:")
@@ -64,16 +102,21 @@ fun addTask() {
     toDo = workList.toList()
 }
 
+/**
+ * Removes the completed tasks from the list
+ * @param toDo The list of Task objects
+ * @return List<Task> updated list of Task objects
+ */
 fun removeCompletedTask() {
     val workList = toDo.toMutableList()
-    for (task in toDo) {
-        if (task.isComplete) {
-            workList.remove(task)
-        }
-    }
+    workList.removeAll { it.isComplete}
     toDo = workList.toList()
     println("Completed tasks removed.")
 }
+
+/**
+ * Displays the user options menu
+ */
 
 fun displayMenu() {
     println("\nWhat would you like to do?")
@@ -84,12 +127,19 @@ fun displayMenu() {
     print(">: ")
 }
 
+/**
+ * Displays the user greeting message and the initial list of tasks
+ */
 fun greetingUser() {
     println("\nWelcome to the Kotlin TO-DO List App!")
     println("_____________________________________")
     println ("Here is your current list of tasks:")
     showList()
 }
+
+/**
+ * Main function to run the program
+ */
 
 fun main(){
     greetingUser()
